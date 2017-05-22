3dcebb85081e6c1d2591181740c5d28b1ffe4d9b
Kailneg <cuenta_buzon@hotmail.com>
2017-05-22 13:23:24 +0200

    Primer commit

--- ProyectoJavaFinal1DAM/src/GUI/Login.java
+++ ProyectoJavaFinal1DAM/src/GUI/Login.java
@@ -8,27 +8,11 @@ public class Login {
 
 	private JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login window = new Login();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the application.
 	 */
 	public Login() {
-		frame.setVisible(true);
 		initialize();
 	}
 
@@ -39,6 +23,7 @@ public class Login {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 }
