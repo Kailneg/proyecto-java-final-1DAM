519f16407b4152951b410f4a3f46729186317ace
Kailneg <cuenta_buzon@hotmail.com>
2017-05-23 12:54:26 +0200

    Aniadido vehiculo, enum EstadoReparacion, enum TipoCombustible

---
+++ ProyectoJavaFinal1DAM/src/enums/TipoCombustible.java
@@ -0,0 +1,5 @@
+package enums;
+
+public enum TipoCombustible {
+	GASOLINA, DIESEL, GLP, HIBRIDO, ELECTRICO
+}
---
+++ ProyectoJavaFinal1DAM/src/models/Vehiculo.java
@@ -0,0 +1,61 @@
+package models;
+
+import enums.TipoCombustible;
+
+public class Vehiculo {
+	
+	//Variables
+	private String matricula, marca, modelo;
+	private int puertas, anioMatriculacion, cv;
+	private TipoCombustible combustible;
+	
+	//Constructor
+	public Vehiculo(String matricula, String marca, String modelo, int puertas, int anioMatriculacion, int cv,
+			TipoCombustible combustible) {
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.puertas = puertas;
+		this.anioMatriculacion = anioMatriculacion;
+		this.cv = cv;
+		this.combustible = combustible;
+	}
+	
+	//Getters - Setters
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public int getPuertas() {
+		return puertas;
+	}
+
+	public int getAnioMatriculacion() {
+		return anioMatriculacion;
+	}
+
+	public int getCv() {
+		return cv;
+	}
+
+	public TipoCombustible getCombustible() {
+		return combustible;
+	}
+
+	public void setCombustible(TipoCombustible combustible) {
+		this.combustible = combustible;
+	}
+	
+
+	
+
+}
