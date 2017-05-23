0baf3004059aeac9a3e73f692fbb427199031d1c
1dam.m <1dam.m@PC106.cenecmalaga.net>
2017-05-23 13:30:41 +0200

    Reparación hecho

--- ProyectoJavaFinal1DAM/src/globals/Globals.java
+++
@@ -1,6 +0,0 @@
-package globals;
-
-public class Globals {
-	public static final String LOGO_PATH = "/res/dast.png";
-	public static final String FONT = "Tahoma";
-}
---
+++ ProyectoJavaFinal1DAM/src/models/Reparacion.java
@@ -0,0 +1,72 @@
+package models;
+
+import java.util.Date;
+
+public class Reparacion {
+
+	private int idReparacion;
+	private Date fechaInicio;
+	private Date fechaFin;
+	private float presupuesto;
+	private String estado;
+	private String comentarios;
+
+	
+	public Reparacion(int idReparacion, Date fechaInicio, Date fechaFin, float presupuesto, String estado,
+			String comentarios) {
+		this.idReparacion = idReparacion;
+		this.fechaInicio = new Date();	
+		this.fechaFin = fechaFin;
+		this.presupuesto = presupuesto;
+		this.estado = estado;
+		this.comentarios = comentarios;
+	}
+
+	// GETTERS
+	public int getIdReparacion() {
+		return idReparacion;
+	}
+
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+	
+	public float getPresupuesto() {
+		return presupuesto;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	
+	public String getComentarios() {
+		return comentarios;
+	}
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	
+	// SETTERS
+	public void setIdReparacion(int idReparacion) {
+		this.idReparacion = idReparacion;
+	}
+
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public void setPresupuesto(float presupuesto) {
+		this.presupuesto = presupuesto;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public void setComentarios(String comentarios) {
+		this.comentarios = comentarios;
+	}
+
+
+}
