73f52ca28fe9c0678b8b1858e32074d5483f9f8d
1dam.m <1dam.m@PC106.cenecmalaga.net>
2017-05-23 13:07:47 +0200

    Modelo cliente hecho

---
+++ ProyectoJavaFinal1DAM/src/models/Cliente.java
@@ -0,0 +1,67 @@
+package models;
+
+public class Cliente {
+
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private String direccion;
+	private long telefono;
+	private String email;
+
+	public Cliente(String dni, String nombre, String apellidos, String direccion, long telefono, String email) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.email = email;
+	}
+
+	// GETTERS
+	public String getDni() {
+		return dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public long getTelefono() {
+		return telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	// SETTERS
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public void setTelefono(long telefono) {
+		this.telefono = telefono;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
