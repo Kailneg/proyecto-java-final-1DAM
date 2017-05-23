450933a58b6b36128cc85f658f0c01d5271459ea
Antonio <Antonio@DESKTOP-6J5EO8Q>
2017-05-23 21:01:07 +0200

    Metodo modelos comentados

--- ProyectoJavaFinal1DAM/src/models/Usuario.java
+++ ProyectoJavaFinal1DAM/src/models/Usuario.java
@@ -19,6 +19,12 @@ public class Usuario {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Cambia la contraseña del usuario.
+	 * @param antiguaContrasenia La contraseña actual, la que queremos cambiar.
+	 * @param nuevaContrasenia La que queremos que sea la nueva contraseña.
+	 * @return True si todo ha salido bien y si no lanzará una excepcion
+	 */
 	public boolean nuevaContrasenia(String antiguaContrasenia, String nuevaContrasenia) {
 		if (this.contrasenia.equals(antiguaContrasenia)){
 			contrasenia = nuevaContrasenia;
@@ -28,6 +34,11 @@ public class Usuario {
 			throw new ContraseniaNoEncontrada();
 	}
 	
+	/**
+	 * Comprueba que la contraseña es correcta
+	 * @param contrasenia La contraseña que queremos validar
+	 * @return True si es correcta si no lanzará una excepcion
+	 */
 	public boolean comprobarContrasenia(String contrasenia){
 		if(this.contrasenia.equals(contrasenia))
 			return true;
