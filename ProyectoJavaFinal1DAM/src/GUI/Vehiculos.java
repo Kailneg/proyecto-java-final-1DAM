a2d6b2da3797cde1467f1b19d121ad0aeee39748
Diana <Diana@HachePe>
2017-05-26 14:16:35 +0200

    GUI.Vehiculos actualizada

--- ProyectoJavaFinal1DAM/src/GUI/Vehiculos.java
+++ ProyectoJavaFinal1DAM/src/GUI/Vehiculos.java
@@ -9,7 +9,9 @@ import javax.swing.JPanel;
 import java.awt.Font;
 import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JTextField;
 
 import controller.ControladorVehiculos;
@@ -23,19 +25,12 @@ public class Vehiculos {
 
 	private JFrame frame;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
 	private JLabel lblNewLabel;
 	private JLabel lblNewLabel_1;
 	private JLabel lblModelo;
 	private JLabel lblPuertas;
-	private JRadioButton radioButton;
-	private JRadioButton radioButton_1;
+	private JRadioButton rb_3puertas;
+	private JRadioButton rb_5puertas;
 	private JLabel lblColor;
 	private JLabel lblAoMatriculacin;
 	private JLabel lblCaballos;
@@ -47,6 +42,16 @@ public class Vehiculos {
 	private JButton button;
 	private JButton button_1;
 	private JButton btnEditar;
+	private JTextField txt_cv;
+	private JTextField txt_marca;
+	private JTextField txt_matricula;
+	private JTextField txt_modelo;
+	private JTextField txt_color;
+	private JTextField txt_combustible;
+	private JTextField txt_potencia;
+	private JComboBox cbox_dia;
+	private JComboBox cbox_mes;
+	private JComboBox cbox_ano;
 	private ControladorVehiculos controladorVehiculos;
 	
 
@@ -86,33 +91,30 @@ public class Vehiculos {
 		lblNewLabel_1 = new JLabel("Marca:");
 		lblModelo = new JLabel("Modelo:");
 		lblPuertas = new JLabel("Puertas:");
-		radioButton = new JRadioButton("3");
-		radioButton_1 = new JRadioButton("5");
+		rb_3puertas = new JRadioButton("3");
+		rb_5puertas = new JRadioButton("5");
 		lblColor = new JLabel("Color:");
-		lblAoMatriculacin = new JLabel("A\u00F1o Matriculaci\u00F3n:");
-		lblCaballos = new JLabel("Caballos:");
-		lblTipoCombustible = new JLabel("Tipo combustible:");
+		lblAoMatriculacin = new JLabel("Matriculaci\u00F3n:");
+		lblCaballos = new JLabel("Potencia:");
+		lblTipoCombustible = new JLabel("Combustible:");
 		btnClientes = new JButton("Clientes");
 		btnRepararVehvulo = new JButton("Reparar Veh\u00EDvulo");
 		btnAtrs = new JButton("Atras");
-		textField = new JTextField();
-		textField.setBounds(96, 30, 86, 20);
-		textField_1 = new JTextField();
-		textField_1.setBounds(82, 59, 100, 20);
-		textField_2 = new JTextField();
-		textField_2.setBounds(84, 92, 98, 20);
-		textField_3 = new JTextField();
-		textField_3.setBounds(84, 166, 86, 20);
-		textField_4 = new JTextField();
-		textField_4.setBounds(151, 204, 86, 20);
-		textField_5 = new JTextField();
-		textField_5.setBounds(96, 245, 86, 20);
-		textField_6 = new JTextField();
-		textField_6.setBounds(151, 283, 86, 20);
 		label = new JLabel("0/0");
 		button = new JButton("<");
 		button_1 = new JButton(">");
 		btnEditar = new JButton("Editar");
+		txt_cv = new JTextField();
+		txt_marca = new JTextField();
+		txt_matricula = new JTextField();
+		txt_modelo = new JTextField();
+		txt_color = new JTextField();
+		txt_combustible = new JTextField();
+		txt_potencia = new JTextField();
+		cbox_dia = new JComboBox();
+		cbox_mes = new JComboBox();
+		cbox_ano = new JComboBox();
+		
 	}
 
 	/**
@@ -123,8 +125,8 @@ public class Vehiculos {
 		frame.getContentPane().add(lblNewLabel_1);
 		frame.getContentPane().add(lblModelo);
 		frame.getContentPane().add(lblPuertas);
-		frame.getContentPane().add(radioButton);
-		frame.getContentPane().add(radioButton_1);
+		frame.getContentPane().add(rb_3puertas);
+		frame.getContentPane().add(rb_5puertas);
 		frame.getContentPane().add(lblColor);
 		frame.getContentPane().add(lblAoMatriculacin);
 		frame.getContentPane().add(lblCaballos);
@@ -133,21 +135,61 @@ public class Vehiculos {
 		frame.getContentPane().add(btnRepararVehvulo);
 		frame.getContentPane().add(btnAtrs);
 		frame.getContentPane().setLayout(null);
-		frame.getContentPane().add(textField);
-		frame.getContentPane().add(textField_1);
-		frame.getContentPane().add(textField_2);
-		frame.getContentPane().add(textField_3);
-		frame.getContentPane().add(textField_4);
-		frame.getContentPane().add(textField_5);
-		frame.getContentPane().add(textField_6);
 		frame.getContentPane().add(label);
 		frame.getContentPane().add(button);
 		frame.getContentPane().add(button_1);
 		frame.getContentPane().add(btnEditar);
 		
-		buttonGroup.add(radioButton);
-		buttonGroup.add(radioButton_1);
+		buttonGroup.add(rb_3puertas);
+		buttonGroup.add(rb_5puertas);
+				
+		txt_cv.setFont(new Font("Tahoma", Font.BOLD, 11));
+		txt_cv.setEditable(false);
+		txt_cv.setText("CV");
+		txt_cv.setBounds(207, 243, 30, 24);
+		frame.getContentPane().add(txt_cv);
+		txt_cv.setColumns(10);	
 		
+		txt_marca.setColumns(10);
+		txt_marca.setBounds(119, 59, 118, 24);
+		frame.getContentPane().add(txt_marca);
+		
+		txt_matricula.setColumns(10);
+		txt_matricula.setBounds(119, 28, 118, 24);
+		frame.getContentPane().add(txt_matricula);	
+		
+		txt_modelo.setColumns(10);
+		txt_modelo.setBounds(119, 91, 118, 24);
+		frame.getContentPane().add(txt_modelo);	
+		
+		txt_color.setColumns(10);
+		txt_color.setBounds(119, 163, 118, 24);
+		frame.getContentPane().add(txt_color);
+		
+		
+		txt_combustible.setColumns(10);
+		txt_combustible.setBounds(119, 281, 118, 24);
+		frame.getContentPane().add(txt_combustible);
+		
+		
+		txt_potencia.setColumns(10);
+		txt_potencia.setBounds(119, 243, 89, 24);
+		frame.getContentPane().add(txt_potencia);
+		
+		
+		cbox_dia.setModel(new DefaultComboBoxModel(new String[] {"D"}));
+		cbox_dia.setBounds(121, 202, 38, 20);
+		frame.getContentPane().add(cbox_dia);
+		
+
+		cbox_mes.setModel(new DefaultComboBoxModel(new String[] {"M"}));
+		cbox_mes.setBounds(158, 202, 38, 20);
+		frame.getContentPane().add(cbox_mes);
+		
+
+		cbox_ano.setModel(new DefaultComboBoxModel(new String[] {"A"}));
+		cbox_ano.setBounds(197, 202, 40, 20);
+		frame.getContentPane().add(cbox_ano);
 	}
 
 	/**
@@ -157,35 +199,32 @@ public class Vehiculos {
 		frame.setBounds(100, 100, 450, 478);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
-		
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblNewLabel.setBounds(36, 29, 64, 19);
 		
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblNewLabel_1.setBounds(36, 62, 53, 19);
 		
-		lblModelo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblModelo.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblModelo.setBounds(36, 92, 64, 19);
 		
-		lblPuertas.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPuertas.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblPuertas.setBounds(36, 137, 64, 14);
 		
-		radioButton.setBounds(96, 135, 38, 23);
+		rb_3puertas.setBounds(132, 133, 38, 23);	
+		rb_5puertas.setBounds(199, 133, 38, 23);
 		
-		radioButton_1.setBounds(132, 135, 38, 23);
-		
-		lblColor.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblColor.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblColor.setBounds(36, 167, 46, 14);
 		
-		lblAoMatriculacin.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblAoMatriculacin.setBounds(36, 205, 121, 14);
+		lblAoMatriculacin.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblAoMatriculacin.setBounds(36, 205, 98, 14);
 		
-		lblCaballos.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblCaballos.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblCaballos.setBounds(36, 246, 64, 14);
 		
-		lblTipoCombustible.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblTipoCombustible.setBounds(36, 284, 113, 14);
+		lblTipoCombustible.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblTipoCombustible.setBounds(36, 284, 73, 14);
 		
 		btnClientes.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnClientes.setBounds(261, 29, 147, 108);
@@ -194,14 +233,7 @@ public class Vehiculos {
 		btnRepararVehvulo.setBounds(261, 167, 147, 108);
 		
 		btnAtrs.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnAtrs.setBounds(261, 310, 147, 76);
-		textField.setColumns(10);
-		textField_1.setColumns(10);
-		textField_2.setColumns(10);
-		textField_3.setColumns(10);
-		textField_4.setColumns(10);
-		textField_5.setColumns(10);
-		textField_6.setColumns(10);
+		btnAtrs.setBounds(261, 335, 147, 76);
 		
 		
 		label.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -213,6 +245,7 @@ public class Vehiculos {
 		
 		btnEditar.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnEditar.setBounds(93, 358, 89, 53);
+		
 	}
 
 	/**
