c48859963e9ca3c826ef202faa821caae8f77a2f
Kailneg <cuenta_buzon@hotmail.com>
2017-05-30 13:18:07 +0200

    Creado controladorElegirVehiculo. ventanaEleccionVehiculo con iconos

--- ProyectoJavaFinal1DAM/src/GUI/VentanaEleccionVehiculo.java
+++ ProyectoJavaFinal1DAM/src/GUI/VentanaEleccionVehiculo.java
@@ -6,56 +6,99 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controller.ControladorElegirVehiculo;
+
 import java.awt.GridLayout;
 import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class VentanaEleccionVehiculo extends JFrame {
-
+	
+	private VentanaEleccionVehiculo frame;
+	private ControladorElegirVehiculo controladorElegirVehiculo;
 	private JPanel contentPane;
+	private JPanel 	panel;
+	private JButton btn_coche;
+	private JButton btn_moto;
+	private JButton btn_camion;
+	private JButton btn_bici;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaEleccionVehiculo frame = new VentanaEleccionVehiculo();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
 	 */
-	public VentanaEleccionVehiculo() {
+	public VentanaEleccionVehiculo(ControladorElegirVehiculo controladorElegirVehiculo) {
+		this.controladorElegirVehiculo = controladorElegirVehiculo;
+		componentsInitializers();
+		componentsProperties();
+		componentsAdders();
+		componentsAdapters();
+	}
+	
+	//Ocultar y mostrar
+	public void mostrarVentana(){
+		frame.setVisible(true);
+	}
+	
+	public void ocultarVentana(){
+		frame.setVisible(false);
+	}
+
+	private void componentsInitializers() {
+		panel = new JPanel();
+		btn_coche = new JButton("");
+		btn_moto = new JButton("");
+		btn_camion = new JButton("");
+		btn_bici = new JButton("");
+	}
+
+	private void componentsProperties() {
+		setResizable(false);
 		setTitle("Eleccion Veh\u00EDculo");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 350);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(new GridLayout(2, 2, 0, 0));
-		
-		JButton btn_coche = new JButton("Coche");
-		panel.add(btn_coche);
-		
-		JButton btn_moto = new JButton("Motocicleta");
-		panel.add(btn_moto);
-		
-		JButton btn_camion = new JButton("Camion");
-		panel.add(btn_camion);
-		
-		JButton btnNewButton_3 = new JButton("Bicicleta");
-		panel.add(btnNewButton_3);
+		btn_coche.setIcon(new ImageIcon(VentanaEleccionVehiculo.class.getResource("/res/icons/coche.png")));
+		btn_moto.setIcon(new ImageIcon(VentanaEleccionVehiculo.class.getResource("/res/icons/moto.png")));
+		btn_camion.setIcon(new ImageIcon(VentanaEleccionVehiculo.class.getResource("/res/icons/camion.png")));
+		btn_bici.setIcon(new ImageIcon(VentanaEleccionVehiculo.class.getResource("/res/icons/bici.png")));
 	}
 
+	private void componentsAdders() {
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.add(btn_coche);
+		panel.add(btn_moto);
+		panel.add(btn_camion);
+		panel.add(btn_bici);
+	}
+
+	private void componentsAdapters() {
+		btn_coche.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+		});
+		btn_moto.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+		});
+		btn_camion.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+		});
+		btn_bici.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+		});
+	}
 }
