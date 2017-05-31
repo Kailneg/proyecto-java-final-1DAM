997cdd1bc2b4d9104d81e50e6ab6376cff6cc3fb
Kailneg <cuenta_buzon@hotmail.com>
2017-05-31 10:59:13 +0200

    Refactorización y javadoc de models.Cliente

--- ProyectoJavaFinal1DAM/src/models/Cliente.java
+++ ProyectoJavaFinal1DAM/src/models/Cliente.java
@@ -11,6 +11,15 @@ public class Cliente {
 	private String email;
 
 	// CONSTRUCTOR
+	/**
+	 * Crea un nuevo objeto de tipo Cliente con los atributos especificados.
+	 * @param dni dni identificador del cliente.
+	 * @param nombre nombre del cliente.
+	 * @param apellidos apellido del cliente
+	 * @param direccion direccion del cliente.
+	 * @param telefono número de teléfono del cliente.
+	 * @param email email del cliente.
+	 */
 	public Cliente(String dni, String nombre, String apellidos, String direccion, long telefono, String email) {
 		this.dni = dni;
 		this.nombre = nombre;
@@ -21,47 +30,91 @@ public class Cliente {
 	}
 
 	// GETTERS
+	/**
+	 * Devuelve el atributo DNI.
+	 * @return atributo dni de la instancia.
+	 */
 	public String getDni() {
 		return dni;
 	}
 
+	/**
+	 * Devuelve el atributo Nombre.
+	 * @return atributo nombre de la instancia.
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Devuelve el atributo Apellidos.
+	 * @return atributo apellidos de la instancia.
+	 */
 	public String getApellidos() {
 		return apellidos;
 	}
 
+	/**
+	 * Devuelve el atributo Dirección.
+	 * @return atributo direccion de la instancia.
+	 */
 	public String getDireccion() {
 		return direccion;
 	}
 
+	/**
+	 * Devuelve el atributo Teléfono.
+	 * @return atributo telefono de la instancia.
+	 */
 	public long getTelefono() {
 		return telefono;
 	}
 
+	/**
+	 * Devuelve el atributo Email.
+	 * @return atributo email de la instancia.
+	 */
 	public String getEmail() {
 		return email;
 	}
 
 	// SETTERS
+	/**
+	 * Fija el atributo nombre de la instancia al valor pasado por parámetros.
+	 * @param nombre el nuevo valor que tendrá el atributo nombre.
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Fija el atributo apellidos de la instancia al valor pasado por parámetros.
+	 * @param apellidos el nuevo valor que tendrá el atributo apellidos.
+	 */
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
 
+	/**
+	 * Fija el atributo direccion de la instancia al valor pasado por parámetros.
+	 * @param direccion el nuevo valor que tendrá el atributo direccion.
+	 */
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
 
+	/**
+	 * Fija el atributo telefono de la instancia al valor pasado por parámetros.
+	 * @param telefono el nuevo valor que tendrá el atributo telefono.
+	 */
 	public void setTelefono(long telefono) {
 		this.telefono = telefono;
 	}
 
+	/**
+	 * Fija el atributo email de la instancia al valor pasado por parámetros.
+	 * @param email el nuevo valor que tendrá el atributo email.
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
@@ -71,5 +124,4 @@ public class Cliente {
 		return "Cliente [dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", direccion=" + direccion
 				+ ", telefono=" + telefono + ", email=" + email + "]";
 	}
-
 }
